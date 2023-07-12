714c59da4c3094239cbe702c79a51b7d81ee6810
Virgil-Tan <tanvirgil1999@gmail.com>
2023-07-11 23:34:02 -0400

    fix bug

---
+++ cmake-build-debug/CMakeFiles/bellmanFord.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bellmanFord.dir/main.cpp.o"
+  "CMakeFiles/bellmanFord.dir/main.cpp.o.d"
+  "bellmanFord"
+  "bellmanFord.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bellmanFord.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <limits>
-#include <ctime>
-#include <atomic>
 #include <pthread.h>
-#include <algorithm>
-#include <iomanip>
+#include <limits>
 
 using namespace std;
 
@@ -22,7 +18,8 @@ struct Graph {
 struct ThreadData {
     int start, end;
     Graph* graph;
-    vector<atomic<int>>* distance;
+    vector<int>* distance;
+    pthread_barrier_t* barrier;
 };
 
 void* bellmanFordPartial(void* args) {
@@ -30,31 +27,42 @@ void* bellmanFordPartial(void* args) {
     int start = data->start;
     int end = data->end;
     Graph* graph = data->graph;
-    vector<atomic<int>>* distance = data->distance;
+    vector<int>* distance = data->distance;
+    pthread_barrier_t* barrier = data->barrier;
 
-    bool changed = true;
-    while (changed) {
-        changed = false;
+    vector<int> local_distance = *distance;
+
+    for (int i = 0; i < graph->V - 1; ++i) {
         for (int j = start; j < end; ++j) {
             int u = graph->edges[j].src;
             int v = graph->edges[j].dest;
             int weight = graph->edges[j].weight;
 
-            int current_dist = (*distance)[v].load();
-            int new_dist = (*distance)[u].load() + weight;
-            while (new_dist < current_dist && !(*distance)[v].compare_exchange_weak(current_dist, new_dist)) {
-                new_dist = (*distance)[u].load() + weight;
-                changed = true;
+            if (local_distance[u] != numeric_limits<int>::max() && local_distance[u] + weight < local_distance[v]) {
+                local_distance[v] = local_distance[u] + weight;
             }
         }
+
+        pthread_barrier_wait(barrier);
+
+        // Combine the results of all threads
+        for (int v = 0; v < graph->V; ++v) {
+            (*distance)[v] = min((*distance)[v], local_distance[v]);
+        }
+
+        pthread_barrier_wait(barrier);
+
+        // Copy the combined results to the local distances for the next iteration
+        local_distance = *distance;
     }
 
     return nullptr;
 }
 
+
 int main() {
-    ifstream infile("/home/virgil/Documents/data/USA-road-d.FLA.gr");
-//    ifstream infile("/home/virgil/CLionProjects/bellmanFord/mapDataTest.txt");
+    ifstream infile("/home/virgil/CLionProjects/bellmanFord/mapDataTest.txt");
+//    ifstream infile("/home/virgil/CLionProjects/bellmanFord/USA-road-d.NY.gr");
     if (!infile) {
         cerr << "Error opening the file." << endl;
         return 1;
@@ -62,17 +70,14 @@ int main() {
 
     int num_nodes, num_edges;
     infile >> num_nodes >> num_edges;
-    num_nodes = num_nodes + 1;
+    num_nodes = num_nodes+1;
     Graph graph;
     graph.V = num_nodes;
     graph.E = num_edges;
 
-    cout << "Node num: " << num_nodes << ", edges: " << num_edges << endl;
-    cout << "Running Bellman Ford's algorithm with starting node 1" << endl;
-
     for (int i = 0; i < num_edges; ++i) {
         int src, dest, weight;
-        char skip_word;
+        char skip_word;;
         infile >> skip_word >> src >> dest >> weight;
         graph.edges.push_back({src, dest, weight});
     }
@@ -83,21 +88,25 @@ int main() {
     double duration;
     start = clock();
 
-    vector<atomic<int>> distance(num_nodes);
-    for (int i = 0; i < num_nodes; ++i) {
-        distance[i] = (i == 1) ? 0 : numeric_limits<int>::max();
-    }
+    vector<int> distance(num_nodes, numeric_limits<int>::max());
+    distance[1] = 0; // Assuming the source node is 0
 
-    int num_threads = 4;
+    int num_threads = 4; // Change this to the desired number of threads
     pthread_t threads[num_threads];
     ThreadData thread_data[num_threads];
+    pthread_barrier_t barrier;
 
-    int edges_per_thread = (num_edges + num_threads - 1) / num_threads;  // ceil division
+    pthread_barrier_init(&barrier, nullptr, num_threads);
+
+
+    int edges_per_thread = (num_edges + num_threads - 1) / num_threads;
+
     for (int i = 0; i < num_threads; ++i) {
         thread_data[i].start = i * edges_per_thread;
         thread_data[i].end = min((i + 1) * edges_per_thread, num_edges);
         thread_data[i].graph = &graph;
         thread_data[i].distance = &distance;
+        thread_data[i].barrier = &barrier;
 
         pthread_create(&threads[i], nullptr, bellmanFordPartial, &thread_data[i]);
     }
@@ -109,16 +118,24 @@ int main() {
     stop = clock();
     duration = ((double)(stop - start)) / CLOCKS_PER_SEC;
 
-    cout << "Execution time: " << duration << endl;
-    cout << "Finished" << endl;
+    cout << duration << endl;
+    cout << "finished" << endl;
+//// Check for negative-weight cycles
+    for (int i = 0; i < graph.E; ++i) {
+        int u = graph.edges[i].src;
+        int v = graph.edges[i].dest;
+        int weight = graph.edges[i].weight;
 
-    cout << "Shortest distances from node 1:" << endl;
-    for (int i = 1; i < num_nodes; ++i) {
-        if (distance[i] != numeric_limits<int>::max()) {
-            cout << "Distance to node " << setw(2) << i << ": " << distance[i] << endl;
-        } else {
-            cout << "Distance to node " << setw(2) << i << ": INF" << endl;
+        if (distance[u] != numeric_limits<int>::max() && distance[u] + weight < distance[v]) {
+            cerr << "Graph contains a negative-weight cycle." << endl;
+            return 1;
         }
+    }
+
+// Print the shortest path distances
+    cout << "Vertex\tDistance from Source" << endl;
+    for (int i = 1; i < num_nodes; ++i) {
+        cout << i << "\t\t" << distance[i] << endl;
     }
 
     return 0;
